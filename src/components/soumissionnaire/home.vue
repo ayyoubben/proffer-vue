42bbaa5e3e58405a662fa79b686460be8f33e32d
ayyoubben <a.benhocine@esi-sba.dz>
2022-06-19 00:05:45 +0100

    nav change

--- src/components/soumissionnaire/home.vue
+++ src/components/soumissionnaire/home.vue
@@ -2,57 +2,40 @@
     <div class="row flex-nowrap">
         <div class="col-2 bg-dark brown">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a @click="$router.push('/')" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline"><img class="img-fluid logo" src="../../assets/logo.png" alt=""></span>
                 </a>
                 <hr>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
+                        <a @click="$router.push('/')" class="nav-link align-middle px-0">
                             <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline text-white ">Home</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#Profile" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <a @click="$router.push('/profile')" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                             <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline text-white ">Profile</span> </a>
-                            <ul class="collapse nav flex-column ms-1" id="Profile" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white ">Profile</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white ">Soumissions</span></a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline text-white ">Sécurité</span></a>
-                            </li>
-                        </ul>
+        
                     </li>
                     <li>
-                        <a href="#Notifications" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <a @click="$router.push('/notifications')" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                             <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline text-white ">Notifications</span> </a>
                     </li>
                     <li>
-                        <a href="#Contact_us" class="nav-link px-0 align-middle">
+                        <a @click="$router.push('/contact')" class="nav-link px-0 align-middle">
                             <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline text-white ">Contact Us</span></a>
                     </li>
                 </ul>
                 <hr>
                 <div class="dropdown pb-4">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="../../assets/AF_281.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                        <span class="d-none d-sm-inline mx-1">loser</span>
+                        <span class="d-none d-sm-inline mx-1">Logout</span>
                     </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a class="dropdown-item" href="#">Nouvelle soumission</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
                 </div>
             </div>
         </div>
+
         <div class="col-10">
         <!-- Account page navigation-->
         <hr class="mt-0 mb-4">
@@ -162,7 +145,7 @@ body{margin-top:20px;
           margin-top: 20px;
           margin-bottom: 20px;
         }
-        .nav-borders .navlink.active {
+        .nav-borders .nav-link.active {
         color: #FF4301;
         border-bottom-color: #FF4301;
         }
